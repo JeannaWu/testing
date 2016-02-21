1c3180c31c88818c8347a26158e06df8b40805de
ZHIYUN WU <wzyunzx@163.com>
2016-02-21 21:12:40 +0000

    Set voting

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get 'static_pages/help'
 
  resources :posts do
+ 	member do
+ 		get "like", to:"posts#upvote"
+ 		get "dislike", to:"posts#downvote"
+ 	end
  	resources :comments
  end
   root 'posts#index'
