e1b6ec7bce0da4ebba0b07fe08aaae4953060ca2
ZHIYUN WU <wzyunzx@163.com>
2016-03-22 13:27:06 +0000

    cahnge database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320201759) do
+ActiveRecord::Schema.define(version: 20160316095753) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -61,17 +61,6 @@ ActiveRecord::Schema.define(version: 20160320201759) do
     t.integer  "category_id",        limit: 4
     t.datetime "approved_at"
   end
-
-  create_table "relationships", force: :cascade do |t|
-    t.integer  "follower_id", limit: 4
-    t.integer  "followed_id", limit: 4
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-  end
-
-  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
-  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
-  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.boolean  "admin",                  limit: 1
